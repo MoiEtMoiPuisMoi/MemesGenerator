e2f67bd644bf5c3d12ba8a8b64a57c39d80361c8
TheCyberDevlopper <56231137+TheCyberDevlopper@users.noreply.github.com>
2021-05-04 20:03:09 +0200

    Delete script.js

--- page/script.js
+++
@@ -1,5 +0,0 @@
-function invite(){window.open("https://www.coolcraft.ovh/memeshub", '_blank')}
-
-function join(){window.open("https://discord.gg/SgWgxzbuCJ", '_blank')}
-
-function opensource(){window.open("https://github.com/TheCyberDevlopper/MemesGenerator", '_blank')}
